a7c6c703054caead1afaae64544355cbdd07f998
Julissa Hash <69886471+yoyachi@users.noreply.github.com>
2021-03-17 17:19:48 -0500

    add update seeds, routes

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -13,34 +13,12 @@ Category.init(
       primaryKey: true,
       autoIncrement: true
     },
-    product_name: {
+    
+    category_name: {
       type: DataTypes.STRING,
       allowNull: false
+    }
     },
-    price: {
-      type: DataTypes.DECIMAL,
-      allowNull: false,
-      validate: {
-        isDecimal: true
-      }
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 10,
-      validate: {
-        isNumeric: true
-      }
-    },
-    category_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'category',
-        key: 'id'
-      }
-    },
-
-  },
   {
     sequelize,
     timestamps: false,
